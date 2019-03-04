10e625d51ad9af0d9fac80200c7b8b93e1e0951e
Chris Chase <cchase@redhat.com>
2019-03-04 15:02:47 -0500

    Small simplifications.

--- gesture_api/apiv1/gesture.py
+++ gesture_api/apiv1/gesture.py
@@ -5,18 +5,6 @@ from flask import jsonify, request, abort
 from gesture_api.apiv1 import apiv1, storage
 
 
-@apiv1.route('/gesture', methods=['GET'])
-def get_gesture():
-
-    gesture_obj = {
-        'label': 'floss',
-        "probability": 0,
-        's3WriteStatus': 'complete'
-    }
-
-    return jsonify(gesture_obj)
-
-
 @apiv1.route('/gesture', methods=['POST'])
 def create_gesture():
     if not request.data:
@@ -27,10 +15,11 @@ def create_gesture():
     probability = 100 if body.get('label') else 0
     object_id = str(uuid.uuid4())
     storage.write_dict(body, f'{label}/{object_id}')
+    print(f'{label}/{object_id}')
+    print(body)
     gesture_obj = {
         'label': label,
-        "probability": probability,
-        'body': body
+        'probability': probability
     }
 
     return jsonify(gesture_obj)
